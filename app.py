f3b8b46db3a54e3957bea39e14750b71c9940357
Fernanda <fernandamarquesluizgaspar@gmail.com>
2026-06-11 16:08:26 -0300

    Finalização da busca dos produtos por categoria.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from model.categorias import recuperar_categorias as rc
+from model.produtos import recuperar_produtos as rp, recuperar_produtos_por_categoria
 
 app = Flask(__name__)
 
@@ -7,6 +8,11 @@ app = Flask(__name__)
 def index():
     categorias = rc()
     return render_template("inicio.html", categorias = categorias)
+
+@app.route("/produtos/<id_categoria>")
+def pagina_produtos(id_categoria):
+    produtos = recuperar_produtos_por_categoria(id_categoria)
+    return render_template("produtos.html", produtos = produtos)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- model/produtos.py
+++ model/produtos.py
@@ -13,3 +13,17 @@ def recuperar_produtos():
     except Exception as erro:
         print(erro)
         return False
+
+def recuperar_produtos_por_categoria(id):
+    """Função criada para buscar os produtos por categoria no banco de dados."""
+
+    try:
+        conexao, cursor = conectar()
+        cursor.execute("SELECT id_produto, nome_produto, preco_produto, descricao_produto, imagem_produto, id_categoria FROM tb_produtos WHERE id_categoria = %s;", (id, ))
+        produtos_por_categoria = cursor.fetchall()
+        conexao.close()
+        return produtos_por_categoria
+    
+    except Exception as erro:
+        print(erro)
+        return False
