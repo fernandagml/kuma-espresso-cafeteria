d56130c3cb23993e2ade570ea0e87aa1718a9751
Jenifer <jenifer.goncalves6@aluno.senai.br>
2026-06-12 10:26:28 -0300

    ADD: Estruturação usuarios

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from model.categorias import recuperar_categorias as rc
 from model.produtos import recuperar_produtos as rp, recuperar_produtos_por_categoria, recuperar_produto
 from model.filtros import recuperar_filtros
-from model.usuarios import cadastro, login
+from model.usuarios import cadastrar, logar
 
 app = Flask(__name__)
 
@@ -21,6 +21,22 @@ def pagina_produtos(id_categoria):
 def pagina_produto(id_produto):
     produto = recuperar_produto(id_produto)
     return render_template("produto_unico.html", produto = produto)
+
+
+@app.route("/cadastro")
+def cadastrando():
+    return render_template("cadastro.html")
+
+
+@app.route("/cadastro/post", methods=["POST"])
+def cadastro_usuario():
+    email_user = request.form.get("email_usuario")
+    senha = request.form.get("senha_usuario")
+
+    if cadastrar(email_user, senha):
+        return render_template("login.html")
+    else:
+        return redirect("/")
 
 
 if __name__ == "__main__":
---
+++ model/usuarios.py
@@ -0,0 +1,24 @@
+from database.conexao import conectar
+
+def cadastrar (nome_usuario, email, telefone, endereco, senha):
+    try:
+        conexao, cursor = conectar()
+        cursor.execute("INSERT INTO tb_usuarios (nome_usuario, email_usuario, telefone_usuario, endereco_usuario, senha_usuario) VALUES (%s, %s, %s, %s, %s);", [nome_usuario, email, telefone, endereco, senha])
+        conexao.commit()
+        conexao.close()
+    except Exception as erro:
+        print(erro)
+        return False
+    
+
+def logar(email:str, senha:str) -> dict:
+    try:
+        conexao, cursor = conectar()
+        cursor.execute("SELECT email_usuario, senha_usuario from tb_usuarios WHERE email_usuario = %s and senha_usuario = %s;", [email, senha])
+        resultado = cursor.fetchone()
+        conexao.close()
+        return resultado
+    
+    except Exception as erro:
+        print(erro)
+        return False
