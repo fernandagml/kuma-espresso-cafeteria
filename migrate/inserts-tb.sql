2f0dc5a81b553e90ebf6beb0afaadc64db78de7a
Jenifer <jenifer.goncalves6@aluno.senai.br>
2026-06-12 16:20:13 -0300

    ADD: Imagens dos doces no banco de dados

--- migrate/inserts-tb.sql
+++ migrate/inserts-tb.sql
@@ -14,7 +14,16 @@ VALUES ("Café Preto", 8.90, "descricao", "../static/img/cafe-preto.jpg", 1),
         ("Milkshake de Chocolate com Nozes", 8.90, "descricao", "../static/img/milkshake-chocolateComNozes.jpg", 4),
         ("Milkshake Meio Amargo", 8.90, "descricao", "../static/img/milkshake-meioAmargo.jpg", 4),
         ("Milkshake de Morango", 8.90, "descricao", "../static/img/milkshake-morango.jpg", 4),
-        ("Milkshake de Oreo", 8.90, "descricao", "../static/img/milkshake-oreo.jpg", 4);
+        ("Milkshake de Oreo", 8.90, "descricao", "../static/img/milkshake-oreo.jpg", 4),
+        ("Brownie", 8.90, "descricao", "../static/img/brownie.jpg", 2),
+        ("Cookies", 8.90, "descricao", "../static/img/cookies.jpg", 2),
+        ("Torta de Morango", 8.90, "descricao", "../static/img/torta-morango.jpg", 2),
+        ("Bomboms da casa", 8.90, "descricao", "../static/img/bomboms-da-casa.jpg", 2),
+        ("Biscoff", 8.90, "descricao", "../static/img/biscoff.jpg", 2),
+        ("Petit Gateau", 8.90, "descricao", "../static/img/petit-gateau.jpg", 2),
+        ("Banofe", 8.90, "descricao", "../static/img/banofe.jpg", 2),
+        ("Red Velvet", 8.90, "descricao", "../static/img/red-velvet.jpg", 2),
+        ("Bolo de Chocolate", 8.90, "descricao", "../static/img/bolo-chocolate.jpg", 2);
         
 INSERT INTO tb_filtros (nome_filtro)
 VALUES ("Sem açucar"), ("Sem lactose"), ("Gelado"), ("Com leite"), ("Milkshake");
