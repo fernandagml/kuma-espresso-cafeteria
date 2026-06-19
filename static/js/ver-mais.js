28c508f2c35451b5e954532b6c2acbb4b9578798
Jenifer <jenifer.goncalves6@aluno.senai.br>
2026-06-19 14:06:00 -0300

    ADD: Comentários no JS

--- static/js/ver-mais.js
+++ static/js/ver-mais.js
@@ -2,9 +2,11 @@ const botao = document.querySelector('.btn-see-more');
 const descricao = document.querySelector('.descricao-oculta');
 
 botao.addEventListener("click", () => {
+    // Escrita aparente (=== block), se sim vai pro none, esconde e muda o texto do botão
     if (descricao.style.display === "block") {
         descricao.style.display = "none";
         botao.textContent = "VER MAIS";
+    // Caso o contrário, exibe e muda o texto do botão
     } else {
         descricao.style.display = "block";
         botao.textContent = "VER MENOS";
