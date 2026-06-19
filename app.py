7006359d69a1160d6ebb555ebbb83d798c0a2b5c
Fernanda <fernandamarquesluizgaspar@gmail.com>
2026-06-19 11:01:34 -0300

    Finalização do backend da busca dos filtros -> fazer correção utilizando API.

--- app.py
+++ app.py
@@ -17,7 +17,10 @@ def index():
 def pagina_produtos(id_categoria):
     produtos = recuperar_produtos_por_categoria(id_categoria)
     filtros = recuperar_filtros()
-    filtros_por_categoria = recuperar_filtros_por_categoria(id_categoria)
+    preco = request.form.get("preco")
+    subcategoria = request.form.get("subcategoria")
+    filtros_por_categoria = recuperar_filtros_por_categoria(id_categoria, preco, subcategoria)
+
     return render_template("produtos.html", produtos = produtos, filtros = filtros, filtros_por_categoria = filtros_por_categoria)
 
 @app.route("/produto/<id_produto>")
@@ -91,12 +94,6 @@ def comentar(id_produto):
 def pagina_catalogo():
     produtos = rp()
     return render_template("catalogo.html", produtos = produtos)
-
-@app.route("/filtros")
-def filtrar_produtos():
-    preco = request.form.get("preco")
-    filtro = request.form.get("")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
